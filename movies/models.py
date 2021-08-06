96dbee6b8e276963a920901dce7d4a3c57a887a6
Alex <temp125@br.ru>
2021-08-06 11:38:47 +0700

    second model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Категории """
+
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Actor(models.Model):
+    """ Актеры и режиссеры """
+    name = models.CharField("Имя", max_length=150)
+    age = models.PositiveSmallIntegerField("Описание")
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="actors/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Актер и режиссер"
+        verbose_name_plural = "Актеры и режиссеры"
